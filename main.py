d982400201bb2bb378f0529078df8f577b7ba29d
Subrata132 <subrata.biswwas@gmail.com>
2022-06-06 17:47:40 +0600

    model updated according to main unet paper.

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+import torch
 from torch.utils.data import DataLoader
 from data_loader.data_loader import CustomDataLoader
+from model.model import UNet
 from utils.visuals import show_result
 
 
 def main():
-    data_loader = CustomDataLoader(
-        image_path='data/train/'
-    )
-    batch_size = 4
-    data_loader = DataLoader(data_loader, batch_size=batch_size)
-    image, label = next(iter(data_loader))
-    print(f'Image type: {type(image)} Image shape: {image.shape}')
-    print(f'Label type: {type(label)} Label shape: {label.shape}')
-    show_result(image, label)
+    # data_loader = CustomDataLoader(
+    #     image_path='data/train/'
+    # )
+    # batch_size = 4
+    # data_loader = DataLoader(data_loader, batch_size=batch_size)
+    # image, label = next(iter(data_loader))
+    # print(f'Image type: {type(image)} Image shape: {image.shape}')
+    # print(f'Label type: {type(label)} Label shape: {label.shape}')
+    # # show_result(image, label)
+    image = torch.rand((4, 1, 572, 572))
+    unet = UNet(input_channel=1)
+    unet(image)
 
 
 if __name__ == '__main__':
--- model/model.py
+++ model/model.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+import torchvision.transforms as transform
 
 
 class ConvBlock(nn.Module):
@@ -8,11 +10,11 @@ class ConvBlock(nn.Module):
             output_channel,
             kernel=3,
             stride=1,
-            padding=1
+            padding=0
     ):
         super().__init__()
         self.conv_block = nn.Sequential(
-            nn.Conv2d(input_channel, output_channel, kernel, stride, padding),
+            nn.Conv2d(input_channel, output_channel, kernel, stride=stride, padding=padding),
             nn.BatchNorm2d(output_channel),
             nn.ReLU(inplace=True),
             nn.Conv2d(output_channel, output_channel, kernel),
@@ -24,11 +26,72 @@ class ConvBlock(nn.Module):
         return x
 
 
+class ConvTranspose2d(nn.Module):
+    def __init__(
+            self,
+            input_channel,
+            output_channel,
+            kernel=3,
+            padding=2,
+            output_padding=0,
+            dilation=1
+    ):
+        super().__init__()
+        self.conv_transpose_2d = nn.ConvTranspose2d(
+            in_channels=input_channel,
+            out_channels=output_channel,
+            kernel_size=kernel,
+            padding=padding,
+            output_padding=output_padding,
+            dilation=dilation
+        )
+
+    def forward(self, x):
+        x = self.conv_transpose_2d(x)
+        return x
+
+
 class UNet(nn.Module):
     def __init__(self, input_channel, retrain=True):
         super().__init__()
-        self.conv1 = ConvBlock(input_channel=input_channel, output_channel=32)
-        self.conv2 = ConvBlock(input_channel=32, output_channel=64)
-        self.conv3 = ConvBlock(input_channel=64, output_channel=128)
-        self.conv4 = ConvBlock(input_channel=128, output_channel=256)
-        
+        self.conv_en1 = ConvBlock(input_channel=input_channel, output_channel=64)
+        self.conv_en2 = ConvBlock(input_channel=64, output_channel=128)
+        self.conv_en3 = ConvBlock(input_channel=128, output_channel=256)
+        self.conv_en4 = ConvBlock(input_channel=256, output_channel=512)
+        self.conv_en5 = ConvBlock(input_channel=512, output_channel=1024)
+        self.max_pool_2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def crop(self, input_tensor, target_tensor):
+        _, _, h, w = target_tensor.shape
+        cropped = transform.CenterCrop([h, w])(input_tensor)
+        return cropped
+
+    def forward(self, x):
+        print("Encoder")
+        ## block1
+
+        conv_en1 = self.conv_en1(x)
+        pool1_en1 = self.max_pool_2x2(conv_en1)
+        print(f'{conv_en1.shape, pool1_en1.shape}')
+
+        ## block2
+        conv_en2 = self.conv_en2(pool1_en1)
+        pool1_en2 = self.max_pool_2x2(conv_en2)
+        print(f'{conv_en2.shape, pool1_en2.shape}')
+
+        ## block3
+        conv_en3 = self.conv_en3(pool1_en2)
+        pool1_en3 = self.max_pool_2x2(conv_en3)
+        print(f'{conv_en3.shape, pool1_en3.shape}')
+
+        ## block4
+        conv_en4 = self.conv_en4(pool1_en3)
+        pool1_en4 = self.max_pool_2x2(conv_en4)
+        print(f'{conv_en4.shape, pool1_en4.shape}')
+
+        ## block5
+        conv_en5 = self.conv_en5(pool1_en4)
+        print(f'{conv_en5.shape}')
+
+
+
