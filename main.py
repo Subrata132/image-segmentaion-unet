878ebe0dc82f8daf592ee9767b4f461b5caf6536
Subrata132 <subrata.biswwas@gmail.com>
2022-06-08 14:34:43 +0600

    test pipeline added.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     parser.add_argument("--epochs", default=5, type=int)
     parser.add_argument("--train_img", default='data/train/', type=str)
     parser.add_argument("--test_img", default='data/val/', type=str)
+    parser.add_argument("--test", default=False, action="store_true")
     args = parser.parse_args()
     trainer = Trainer(
         batch_size=args.batch_size,
@@ -15,7 +16,7 @@ def main():
         train_image_path=args.train_img,
         test_image_path=args.test_img
     )
-    trainer.train()
+    trainer.train(test=args.test)
 
 
 if __name__ == '__main__':
--- trainer.py
+++ trainer.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision.transforms as transform
+import matplotlib.pyplot as plt
 from data_loader.data_loader import CustomDataLoader
 from model.modified_unet import ModifiedUNet
 from utils.visuals import show_result
@@ -28,76 +29,112 @@ class Trainer:
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.show = show
 
-    def train(self):
+    def train(self, test=False):
         image_transform = transform.Compose(
             [transform.ToTensor()]
         )
         label_transform = transform.Compose(
             [transform.ToTensor()]
         )
-        data_loader = CustomDataLoader(
-            image_path=self.train_image_path,
-            image_transformer=image_transform,
-            label_transformer=label_transform
-        )
-        no_train = int(len(data_loader) * 0.85)
-        no_valid = len(data_loader) - no_train
-        train_data_loader, validation_data_loader = torch.utils.data.random_split(
-            dataset=data_loader,
-            lengths=[no_train, no_valid]
-        )
-        train_data_loader = DataLoader(
-            dataset=train_data_loader,
-            batch_size=self.batch_size
-        )
-        validation_data_loader = DataLoader(
-            dataset=validation_data_loader,
-            batch_size=self.batch_size
-        )
-        model = ModifiedUNet(input_channel=3).to(device=self.device)
-        loss_func = nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
-        all_train_loss = []
-        validation_loss = []
-        for i in range(self.epochs):
-            print(f'Epoch: {i + 1}')
-            train_loss = 0
-            val_loss = 0
-            print('Training.............')
-            for image, label in tqdm(train_data_loader):
-                optimizer.zero_grad()
-                image = image.to(device=self.device)
-                label = label.to(device=self.device)
-                output = model(image)
-                loss = loss_func(output, label)
-                loss.backward()
-                optimizer.step()
-                train_loss = train_loss + loss.item()
-            all_train_loss.append(train_loss / len(train_data_loader))
-            if self.show:
-                show_result(image, output, label)
-            print('Validating...........')
-            for image, label in tqdm(validation_data_loader):
-                image = image.to(device=self.device)
-                label = image.to(device=self.device)
-                output = model(image)
-                loss = loss_func(output, label)
-                val_loss = val_loss + loss.item()
-            validation_loss.append(val_loss / len(validation_data_loader))
-            print(f'Epoch: {i + 1} | Train loss: {all_train_loss[-1]} | Validation loss: {validation_loss[-1]}')
-            print('')
-        model_state = {
-            "state_dict": model.state_dict()
-        }
-        loss_dict = {
-            'train_loss': all_train_loss,
-            'val_loss': validation_loss
-        }
-        if not os.path.isdir('saved_weights'):
-            os.makedirs('saved_weights')
+        if not test:
+            data_loader = CustomDataLoader(
+                image_path=self.train_image_path,
+                image_transformer=image_transform,
+                label_transformer=label_transform
+            )
+            no_train = int(len(data_loader) * 0.85)
+            no_valid = len(data_loader) - no_train
+            train_data_loader, validation_data_loader = torch.utils.data.random_split(
+                dataset=data_loader,
+                lengths=[no_train, no_valid]
+            )
+            train_data_loader = DataLoader(
+                dataset=train_data_loader,
+                batch_size=self.batch_size
+            )
+            validation_data_loader = DataLoader(
+                dataset=validation_data_loader,
+                batch_size=self.batch_size
+            )
+            model = ModifiedUNet(input_channel=3).to(device=self.device)
+            loss_func = nn.MSELoss()
+            optimizer = torch.optim.Adam(model.parameters(), lr=self.lr)
+            all_train_loss = []
+            validation_loss = []
+            for i in range(self.epochs):
+                print(f'Epoch: {i + 1}')
+                train_loss = 0
+                val_loss = 0
+                print('Training.............')
+                for image, label in tqdm(train_data_loader):
+                    optimizer.zero_grad()
+                    image = image.to(device=self.device)
+                    label = label.to(device=self.device)
+                    output = model(image)
+                    loss = loss_func(output, label)
+                    loss.backward()
+                    optimizer.step()
+                    train_loss = train_loss + loss.item()
+                all_train_loss.append(train_loss / len(train_data_loader))
+                if self.show:
+                    show_result(image, output, label)
+                print('Validating...........')
+                for image, label in tqdm(validation_data_loader):
+                    image = image.to(device=self.device)
+                    label = label.to(device=self.device)
+                    output = model(image)
+                    loss = loss_func(output, label)
+                    val_loss = val_loss + loss.item()
+                validation_loss.append(val_loss / len(validation_data_loader))
+                print(f'Epoch: {i + 1} | Train loss: {all_train_loss[-1]} | Validation loss: {validation_loss[-1]}')
+                print('')
+            model_state = {
+                "state_dict": model.state_dict()
+            }
+            loss_dict = {
+                'train_loss': all_train_loss,
+                'val_loss': validation_loss
+            }
+            if not os.path.isdir('saved_weights'):
+                os.makedirs('saved_weights')
 
-        torch.save(model_state, f'saved_weights/saved_model_{self.epochs}.pth')
-        with open('saved_weights/loss_info.json', 'w') as file:
-            json.dump(loss_dict, file)
-        file.close()
+            torch.save(model_state, f'saved_weights/saved_model_{self.epochs}.pth')
+            with open('saved_weights/loss_info.json', 'w') as file:
+                json.dump(loss_dict, file)
+            file.close()
+        else:
+            test_data_loader = CustomDataLoader(
+                image_path=self.test_image_path,
+                image_transformer=image_transform,
+                label_transformer=label_transform
+            )
+            test_data_loader = DataLoader(
+                dataset=test_data_loader,
+                batch_size=self.batch_size
+            )
+            model = ModifiedUNet(input_channel=3).to(device=self.device)
+            model.load_state_dict(torch.load('saved_weights/saved_model_120.pth')['state_dict'])
+            model.eval()
+            fig, axes = plt.subplots(self.batch_size, 3, figsize=(18, 7))
+            with torch.no_grad():
+                for image, label in test_data_loader:
+                    image = image.to(device=self.device)
+                    label = label.to(device=self.device)
+                    output = model(image)
+                    images = image.cpu()
+                    outputs = output.cpu()
+                    labels = label.cpu()
+                    k = 0
+                    for i, ax in enumerate(axes.ravel()):
+                        if i % 3 == 0:
+                            ax.imshow(images[k].detach().permute(1, 2, 0).numpy())
+                        elif i % 3 == 1:
+                            ax.imshow(labels[k].detach().permute(1, 2, 0).numpy())
+                        else:
+                            ax.imshow(outputs[k].detach().permute(1, 2, 0).numpy())
+                            k += 1
+                        ax.set_xticks([])
+                        ax.set_yticks([])
+                    plt.tight_layout()
+                    plt.pause(0.5)
 
